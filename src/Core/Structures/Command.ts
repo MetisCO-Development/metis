4bf504d1eaa7e7d5e60f1e367bc02ad5d446bbee
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-08 20:28:15 +0000

    Command class updates

--- src/Core/Structures/Command.ts
+++ src/Core/Structures/Command.ts
@@ -4,27 +4,22 @@ import {ICommandContext} from "../../types";
 export class Command { 
     name: string; 
     id: string; 
+    module: string; 
     aliases: Array<string>; 
-    requiredGuilds: Array<string>;
-    requiredRoles: Array<string>;
-    requiredUsers: Array<string>;
     helpInfo: string; 
     helpUsage: string;
     helpExample: string;
-    commandType: string; 
     constructor(data: Partial<Command>){ 
         this.name = data.name || 'unnamed'
-        this.aliases = data.aliases || []; 
         this.id = data.name; 
+        this.module = data.module || 'default'
+        this.aliases = data.aliases || ['None']; 
 
-        this.requiredGuilds = data.requiredGuilds || []; 
-        this.requiredRoles = data.requiredRoles || []; 
-        this.requiredUsers = data.requiredUsers || []; 
 
         this.helpInfo = data.helpInfo || 'someone waz lazy *yawn*'; 
         this.helpUsage = data.helpUsage 
-        
-        this.commandType = data.commandType || 'default'
+        this.helpExample = data.helpExample 
+    
     }
     async execute(metis: Metis, ctx: ICommandContext): Promise<unknown>{
         return 'Unimplemented'
