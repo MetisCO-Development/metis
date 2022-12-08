3120aa53e850c7740c1d5af56d50d5d762ee098c
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-08 20:29:14 +0000

    add permissions to command class

--- src/Core/Structures/Command.ts
+++ src/Core/Structures/Command.ts
@@ -1,11 +1,13 @@
 import {Message, TextableChannel} from "eris"; 
 import {Metis} from "../../main"; 
 import {ICommandContext} from "../../types";
+
 export class Command { 
     name: string; 
     id: string; 
     module: string; 
     aliases: Array<string>; 
+    permissions: string;
     helpInfo: string; 
     helpUsage: string;
     helpExample: string;
@@ -14,8 +16,7 @@ export class Command {
         this.id = data.name; 
         this.module = data.module || 'default'
         this.aliases = data.aliases || ['None']; 
-
-
+        this.permissions = data.permissions || 'User'
         this.helpInfo = data.helpInfo || 'someone waz lazy *yawn*'; 
         this.helpUsage = data.helpUsage 
         this.helpExample = data.helpExample 
