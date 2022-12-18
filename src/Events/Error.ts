8491b372a5bbe1b51e92c07ec65a8e6b505d60eb
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-18 06:12:33 +0000

    error handling

--- src/Events/Error.ts
+++ src/Events/Error.ts
@@ -4,11 +4,20 @@ let logTime = new Date().toLocaleTimeString('en-us', {timeZone: 'America/New_Yor
 let logDate = new Date().toLocaleDateString();  
 
 metis.client.on('error', async (err: Error, id: Number) => { 
-     metis.client.executeWebhook(config.readyWebhookID, config.readyWebhook, { 
-        embeds: [{
-            color: metis.colors.red,
-            description: `\`${logDate}  ${logTime}\` <@!${metis.client.user.id}> [ERROR] Shard: \`${id}\`\n${err.name}: ${err.message}`
-        }]
-    })
+    if (err) { 
+        metis.client.executeWebhook(config.readyWebhookID, config.readyWebhook, { 
+            embeds: [{
+                color: metis.colors.red,
+                description: `\`${logDate}  ${logTime}\` <@!${metis.client.user.id}> [ERROR] Shard: \`${id}\`\n${err.name}: ${err.message}`
+            }]
+        }).catch((error: Error) => {})
+    } else { 
+        metis.client.executeWebhook(config.readyWebhookID, config.readyWebhook, { 
+            embeds: [{
+                color: metis.colors.red,
+                description: `\`${logDate}  ${logTime}\` <@!${metis.client.user.id}> [ERROR] Shard: \`${id}\``
+            }]
+        }).catch((error: Error) => {})
+    }
 })
 
