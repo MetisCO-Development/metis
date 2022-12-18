265a8adb103ba280e98ebe04a02b3ffcab3b0b15
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-18 05:57:12 +0000

    Attempt to catch some errors

--- src/Events/ShardDisconnect.ts
+++ src/Events/ShardDisconnect.ts
@@ -8,7 +8,7 @@ metis.client.on('shardDisconnect', async (err: Error, id: Number) => {
     metis.client.executeWebhook(config.readyWebhookID, config.readyWebhook, { 
         embeds: [{
             color: metis.colors.red,
-            description: `\`${logDate}  ${logTime}\` <@!${metis.client.user.id}> [SHARD DISCONNECT] Shard: \`${id}\`\n${err.message}`
+            description: `\`${logDate}  ${logTime}\` <@!${metis.client.user.id}> [SHARD DISCONNECT] Shard: \`${id}\`\n${err.name}: ${err.message}`
         }]
-    })
+    }).catch((error: Error) => {})
 })
