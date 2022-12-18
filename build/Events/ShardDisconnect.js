ef816cd87d166ba912be0da15b43e68cd88d1ad3
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-18 05:47:16 +0000

    err

--- build/Events/ShardDisconnect.js
+++ build/Events/ShardDisconnect.js
@@ -8,7 +8,7 @@ main_1.Metis.client.on('shardDisconnect', async (err, id) => {
     main_1.Metis.client.executeWebhook(config.readyWebhookID, config.readyWebhook, {
         embeds: [{
                 color: main_1.Metis.colors.red,
-                description: `\`${logDate}  ${logTime}\` <@!${main_1.Metis.client.user.id}> [SHARD DISCONNECT] Shard: \`${id}\`\n${err.name}: ${err.message}`
+                description: `\`${logDate}  ${logTime}\` <@!${main_1.Metis.client.user.id}> [SHARD DISCONNECT] Shard: \`${id}\`\n${err.message}`
             }]
     });
 });
